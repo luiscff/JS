e98233b322e69db782590a607d9828a04db3da3a
Luís <a100549@alunos.uminho.pt>
2023-07-05 16:06:32 +0100

    Add more info to display and Add style sheet

--- weather-app/main.js
+++ weather-app/main.js
@@ -1,10 +1,9 @@
 
 function getCity() { //gets the city, state and country from the form and calls the functions in apiCall.js
     const city = document.getElementById('city').value;
-    const state = document.getElementById('state').value;
     const country = document.getElementById('country').value;
-    console.log(city, state, country);
-    fetchCoordsOfCity(city, state, country) //gets the coordinates of the city
+    console.log(city, country);
+    fetchCoordsOfCity(city, country) //gets the coordinates of the city
   .then((coords) => { //then uses the coordinates to get the weather info
     const { lat, lon } = coords;
     fetchWeatherOfCoords(lat, lon);
@@ -17,6 +16,47 @@ function getCity() { //gets the city, state and country from the form and calls 
 function displayWeatherInfo(json) {
   const display = document.getElementById('display');
   console.log(json); //para ver json na consola (ajuda a ver a info que tem no json)
-  display.innerHTML = json.name + " " + json.main.temp + "ºC";
-    //TODO display more info in the website
+  display.innerHTML = json.name + "<br>" + json.main.temp + "ºC";
+  display.innerHTML += "<br>" + json.weather[0].description; 
+  display.innerHTML += "<br>Feels like: " + json.main.feels_like + "ºC";
+  display.innerHTML += "<br>Humidity: " + json.main.humidity + "%";
+  display.innerHTML += "<br>Cloudiness: " + json.clouds.all + "%";
+  display.innerHTML += "<br>Country: " + json.sys.country;
+  display.innerHTML += "<br>Sunrise: " + new Date(json.sys.sunrise * 1000).toLocaleTimeString();
+  display.innerHTML += "<br>Sunset: " + new Date(json.sys.sunset * 1000).toLocaleTimeString();
+  display.innerHTML += "<br>Time: " + new Date(json.dt * 1000).toLocaleTimeString();
+}
+
+// this function gets the coordinates of the city using the OpenWeatherMap API
+function fetchCoordsOfCity(city, state, country) {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  return fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",," + country + "&limit=1&appid=6f02f16fbb21d6c9f5b0d019e62daf21", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      if (result.length > 0) {
+        var { lat, lon } = result[0];
+        console.log(lat, lon);
+        return { lat, lon };
+      } else {
+        throw new Error('No location found');
+      }
+    })
+    .catch(error => console.log('ERROR', error));
+}
+
+// this function gets the weather info using the OpenWeatherMap API
+function fetchWeatherOfCoords(lat, lon) {
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+
+  fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=6f02f16fbb21d6c9f5b0d019e62daf21", requestOptions)
+    .then(response => response.json())
+    .then(result => displayWeatherInfo(result))
+    .catch(error => console.log('error', error));
 }
